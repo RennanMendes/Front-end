518746784f7cababe3392c51e2a7b13dcbda74b2
rennan abreu <amrennan@gmail.com>
2022-03-04 20:37:05 -0300

    Componente inicio sem consumo

--- BlogPessoal/src/app/menu/menu.component.ts
+++ BlogPessoal/src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nome =environment.nome
+  foto = environment.foto
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+  }
+
+  sair(){
+    this.router.navigate(['/entrar'])
+    environment.token = ''
+    environment.nome = ''
+    environment.foto = ''
+    environment.id = 0
+
   }
 
 }
--- BlogPessoal/src/app/service/auth.service.ts
+++ BlogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -13,15 +14,23 @@ export class AuthService {
     private http: HttpClient
   ) { }
   
-
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)  
-  
 
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token !=''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
